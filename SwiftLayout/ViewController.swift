862580eb7564c1a26afb885dfc7ff9106128a130
MangoFang <100mango@gmail.com>
2015-10-10 12:22:04 +0800

    init

---
+++ SwiftLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLayout
+//
+//  Created by Mango on 15/10/10.
+//  Copyright © 2015年 Mango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
